362c0c0d5ef4a05c279838e3c69236e2aafb9335
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2015-09-02 01:33:37 +0200

    beautify

--- app.js
+++ app.js
@@ -1,55 +1,53 @@
 var express = require('express'),
-app = express(),
-mysql = require('mysql'),
-bodyParser = require('body-parser'),
+  app = express(),
+  mysql = require('mysql'),
+  bodyParser = require('body-parser'),
 
-mysqlPool = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'user',
-  database: 'conquest'
-});
+  mysqlPool = mysql.createPool({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'user',
+    database: 'conquest'
+  });
 
 app.use(bodyParser.json());
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.status("200").send('ConquestRestAPI is running...');
 });
 
-app.get('/api/patients', function(req, res){
+app.get('/api/patients', function(req, res) {
   //Return patient list
   res.status("200").send('Return patient list');
 });
 
-app.post('/api/patients', function(req, res){
+app.post('/api/patients', function(req, res) {
   //Create patient
   mysqlPool.getConnection(function(err, connection) {
-		if (err) {
-			console.log(err);
+    if (err) {
+      console.log(err);
       res.status("500").send(err.toString());
-		}
-		else {
-				connection.query('INSERT INTO DICOMWorkList SET ?', req.body, function(err, result) {
-					if (err) {
-						console.log(err);
-            res.status("500").send(err.toString());
-					}
-					else {
-						res.status("201").end();
-					}
-				});
-			connection.release();
-		}
-	});
+    } else {
+      connection.query('INSERT INTO DICOMWorkList SET ?', req.body, function(err, result) {
+        if (err) {
+          console.log(err);
+          res.status("500").send(err.toString());
+        } else {
+          res.status("201").end();
+        }
+      });
+      connection.release();
+    }
+  });
 });
 
-app.get('/api/patients/:Id', function(req, res){
+app.get('/api/patients/:Id', function(req, res) {
   //Return patient data
 });
-app.put('/api/patients/:Id', function(req, res){
+app.put('/api/patients/:Id', function(req, res) {
   //Update patient data
 });
-app.delete('/api/patients/:Id', function(req, res){
+app.delete('/api/patients/:Id', function(req, res) {
   //Delete patient
 });
 
