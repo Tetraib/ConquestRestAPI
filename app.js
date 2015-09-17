819eb46665ea20772d54fc06039af7bdecd9be79
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2015-09-17 02:17:50 +0200

    Dicom Xml parsing

--- app.js
+++ app.js
@@ -4,40 +4,25 @@ var express = require('express'),
   bodyParser = require('body-parser'),
   gcloud = require('gcloud'),
   fs = require('fs'),
+  saxStream = require("sax").createStream(false, {
+    trim: true
+  }),
   process = require('child_process'),
 
-  gcs = gcloud.storage({
-    keyFilename: './CloudDicom-1f0d0f461c12.json',
-    projectId: 'axiomatic-math-616'
-  }),
-  bucket = gcs.bucket('dicom'),
-
+  //Conquest Mysql database
   mysqlPool = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
     password: 'user',
     database: 'conquest'
+  }),
+
+  //Cloud storage connection param
+  gcs = gcloud.storage({
+    keyFilename: './CloudDicom-1f0d0f461c12.json',
+    projectId: 'axiomatic-math-616'
   });
-
 app.use(bodyParser.json());
-
-// To use with mirth
-/*
-app.use(function(req, res, next) {
-	if (req.is('text/*')) {
-		req.text = '';
-		req.setEncoding('utf8');
-		req.on('data', function(chunk) {
-			req.text += chunk;
-		});
-		req.on('end', next);
-	}
-	else {
-		next();
-	}
-});
-*/
-
 //
 //
 //Routes to handles worklist
@@ -55,12 +40,12 @@ app.post('/v1/patients', function(req, res) {
   mysqlPool.getConnection(function(err, connection) {
     if (err) {
       console.log(err);
-      res.status("500").send(err.toString());
+      res.status("500").end();
     } else {
       connection.query('INSERT INTO DICOMWorkList SET ?', req.body, function(err, result) {
         if (err) {
           console.log(err);
-          res.status("500").send(err.toString());
+          res.status("500").end();
         } else {
           res.status("201").end();
         }
@@ -84,13 +69,53 @@ app.delete('/v1/patients/:Id', function(req, res) {
 //
 // Routes to handle Forward DICOM
 app.post('/v1/dicoms/', function(req, res) {
-  console.log(req.body.file);
-  var ls = process.spawn('./dcmj2pnm', ["+oj",'pano.dcm']);
-  var remoteWriteStream = bucket.file('pano.jpg').createWriteStream();
-  ls.stdout.pipe(remoteWriteStream);
-  ls.stdout.on('end', function() {
-    res.status("200").end();
+  //
+  // Read xml data from dicom
+  var dcm2xml = process.spawn('./dcm2xml', [req.body.file]),
+    currentNode,
+    dicomjson = {};
+  // Parse xml data to send only usefull info
+  dcm2xml.stdout.pipe(saxStream);
+  dcm2xml.on('error', function(err) {
+    console.log(err);
+    res.status("500").end();
+  });
+  saxStream.on("error", function(e) {
+    console.error("error!", e);
+    // clear the error
+    this._parser.error = null;
+    this._parser.resume();
+  });
+  // Make an json from xml
+  saxStream.on("opentag", function(node) {
+    currentNode = node.attributes.NAME;
+  });
+  saxStream.on("text", function(text) {
+    dicomjson[currentNode] = text;
+  });
+  saxStream.on("end", function() {
+    console.log(dicomjson);
+  });
+  //
+  // Read image from dicom
+  var dcmj2pnm = process.spawn('./dcmj2pnm', ['--quiet','--write-jpeg', '--compr-quality', '50', req.body.file]),
+    // Send image to cloud storage
+    bucket = gcs.bucket('dicom'),
+    remoteWriteStream = bucket.file(req.body.file + ".jpg").createWriteStream();
+  dcmj2pnm.stdout.pipe(remoteWriteStream);
+  //
+  dcmj2pnm.on('error', function(err) {
+    console.log(err);
+    res.status("500").end();
+  });
+  remoteWriteStream.on('error', function(err) {
+    console.log(err);
+    res.status("500").end();
+  });
+  // Send 201 when all done
+  remoteWriteStream.on('finish', function() {
+    res.status("201").end();
   });
 });
 
-app.listen(8080, '192.168.1.166');
+app.listen(8080);
