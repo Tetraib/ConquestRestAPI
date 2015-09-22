e0c7101a841e9fd871aa5159701d8a79d85ccf25
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2015-09-22 03:14:01 +0200

    Forwarding dicom

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ var express = require('express'),
   app = express(),
   mysql = require('mysql'),
   bodyParser = require('body-parser'),
+  request = require('request'),
   gcloud = require('gcloud'),
   fs = require('fs'),
-  saxStream = require("sax").createStream(false, {
-    trim: true
-  }),
+  sax = require("sax"),
   process = require('child_process'),
 
   //Conquest Mysql database
@@ -23,6 +22,33 @@ var express = require('express'),
     projectId: 'axiomatic-math-616'
   });
 app.use(bodyParser.json());
+//
+//
+//Function to convert Xml dicom stream to json
+var dcmxml2json = function(xmlStreamIn, callback) {
+  var currentNode = "",
+    outDicomJson = {},
+    saxStream = sax.createStream(false, {
+      trim: true
+    });
+  xmlStreamIn.pipe(saxStream);
+  saxStream.on("error", function(err) {
+    console.error("error!", err);
+    // clear the error
+    this._parser.error = null;
+    this._parser.resume();
+  });
+  // Make an json from xml to send only usefull info
+  saxStream.on("opentag", function(node) {
+    currentNode = node.attributes.NAME;
+  });
+  saxStream.on("text", function(text) {
+    outDicomJson[currentNode] = text;
+  });
+  saxStream.on("end", function() {
+    callback(outDicomJson);
+  });
+};
 //
 //
 //Routes to handles worklist
@@ -54,7 +80,6 @@ app.post('/v1/patients', function(req, res) {
     }
   });
 });
-
 app.get('/v1/patients/:Id', function(req, res) {
   //Return patient data
 });
@@ -64,58 +89,49 @@ app.put('/v1/patients/:Id', function(req, res) {
 app.delete('/v1/patients/:Id', function(req, res) {
   //Delete patient
 });
-
 //
 //
 // Routes to handle Forward DICOM
 app.post('/v1/dicoms/', function(req, res) {
-  //
   // Read xml data from dicom
-  var dcm2xml = process.spawn('./dcm2xml', [req.body.file]),
-    currentNode,
-    dicomjson = {};
-  // Parse xml data to send only usefull info
-  dcm2xml.stdout.pipe(saxStream);
+  var dicomFile = req.body.file,
+    dicomDataUrl = 'https://dicomwebpacs-tetraib-1.c9.io/v1/images/',
+    dcm2xml = process.spawn('./dcm2xml', [dicomFile]);
   dcm2xml.on('error', function(err) {
     console.log(err);
     res.status("500").end();
   });
-  saxStream.on("error", function(e) {
-    console.error("error!", e);
-    // clear the error
-    this._parser.error = null;
-    this._parser.resume();
+  dcmxml2json(dcm2xml.stdout, function(dcmjson) {
+    request.post(dicomDataUrl, {
+      json: dcmjson
+    }, function(error, response, body) {
+      if(error){
+        console.log(error);
+        res.status("500").end();
+      }else if (response.statusCode == 201) {
+        // Read image from dicom
+        var dcmj2pnm = process.spawn('./dcmj2pnm', ['--quiet', '--write-jpeg', '--compr-quality', '50', dicomFile]),
+          // Send image to cloud storage
+          bucket = gcs.bucket('dicom'),
+          remoteWriteStream = bucket.file(dicomFile + ".jpg").createWriteStream();
+        dcmj2pnm.stdout.pipe(remoteWriteStream);
+        //
+        dcmj2pnm.on('error', function(err) {
+          console.log(err);
+          res.status("500").end();
+        });
+        remoteWriteStream.on('error', function(err) {
+          console.log(err);
+          res.status("500").end();
+        });
+        // Send 201 when all done
+        remoteWriteStream.on('finish', function() {
+          res.status("201").end();
+        });
+      }
+    });
   });
-  // Make an json from xml
-  saxStream.on("opentag", function(node) {
-    currentNode = node.attributes.NAME;
-  });
-  saxStream.on("text", function(text) {
-    dicomjson[currentNode] = text;
-  });
-  saxStream.on("end", function() {
-    console.log(dicomjson);
-  });
-  //
-  // Read image from dicom
-  var dcmj2pnm = process.spawn('./dcmj2pnm', ['--quiet','--write-jpeg', '--compr-quality', '50', req.body.file]),
-    // Send image to cloud storage
-    bucket = gcs.bucket('dicom'),
-    remoteWriteStream = bucket.file(req.body.file + ".jpg").createWriteStream();
-  dcmj2pnm.stdout.pipe(remoteWriteStream);
-  //
-  dcmj2pnm.on('error', function(err) {
-    console.log(err);
-    res.status("500").end();
-  });
-  remoteWriteStream.on('error', function(err) {
-    console.log(err);
-    res.status("500").end();
-  });
-  // Send 201 when all done
-  remoteWriteStream.on('finish', function() {
-    res.status("201").end();
-  });
+
 });
 
 app.listen(8080);
