ef18e76fbed336cd46f70150d6339ebdc8c6d0e5
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2015-10-07 01:29:49 +0200

    Rewrite image and json POST

--- app.js
+++ app.js
@@ -64,18 +64,26 @@ app.delete('/v1/patients/:Id', function(req, res) {
  * Route to Forward DICOM
  */
 app.post('/v1/dicoms/', function(req, res) {
-  //
-  // Read xml data from dicom
   var dicomFile = req.body.file,
+  // Options for request http POST xml
+    postJsonOpt = {
+      url: 'https://dicomwebpacs-tetraib-1.c9.io/v1/images/',
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      }
+    },
+    // Read xml data from dicom
     dcm2xml = process.spawn('./dcm2xml', ['--quiet', '+Ca', 'latin-1', dicomFile]);
 
   dcm2xml.on('error', function(err) {
     console.log(err);
   });
-
+// New XML parser
   var parser = new expat.Parser('UTF-8'),
     currentAttrs = '',
     first = true,
+    // Read Stream to push JSON from XML
     rs = new stream.Readable();
   rs._read = function() {};
   rs.on('error', function(err) {
@@ -83,11 +91,13 @@ app.post('/v1/dicoms/', function(req, res) {
   });
 
   parser.on('startElement', function(name, attrs) {
+    // Get current XML NODE
     if (attrs.name) {
       currentAttrs = attrs.name.trim();
     }
   });
   parser.on('text', function(text) {
+    // Push JSON to read stream
     if (currentAttrs && text && text.trim()) {
       if (first) {
         rs.push('{' + JSON.stringify(currentAttrs) + ' : ' + JSON.stringify(text));
@@ -106,38 +116,38 @@ app.post('/v1/dicoms/', function(req, res) {
   parser.on('error', function(err) {
     console.error(err);
   });
-  var postJsonOpt = {
-    url: 'https://dicomwebpacs-tetraib-1.c9.io/v1/images/',
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json'
-    }
-  };
-
 
   dcm2xml.stdout.pipe(parser);
-  rs.pipe(request(postJsonOpt));
-
-  /**
-   *
-   * Treat image
-   */
-
-  var putImageOpt = {
-    url: 'https://dicomwebpacs-tetraib-1.c9.io/v1/images/1010/',
-    method: 'PUT'
-  };
-  var dcmj2pnm = process.spawn('./dcmj2pnm', ['--quiet', '--write-jpeg', '--compr-quality', '90', dicomFile]),
-    myJpegTranslator = new JpegTran(['-progressive', '-optimize', '-copy', 'none']);
-
-  dcmj2pnm.on('error', function(err) {
+  rs.pipe(request(postJsonOpt).on('error', function(err) {
     console.log(err);
-  });
-  myJpegTranslator.on('error', function(err) {
-    console.log(err);
-  });
+  }).on('response', function(response) {
+    /**
+     *
+     * Treat image
+     */
+    if(response.statusCode=='201'){
+      // Options for request http PUT image
+      var putImageOpt = {
+          url: 'https://dicomwebpacs-tetraib-1.c9.io/v1/images/123/',
+          method: 'PUT'
+        },
+        // Create jpeg from dicom image
+        dcmj2pnm = process.spawn('./dcmj2pnm', ['--quiet', '--write-jpeg', '--compr-quality', '90', dicomFile]),
+        // Optimize jpeg with jpegtran
+        myJpegTranslator = new JpegTran(['-progressive', '-optimize', '-copy', 'none']);
 
-  dcmj2pnm.stdout.pipe(myJpegTranslator).pipe(request(putImageOpt));
+      dcmj2pnm.on('error', function(err) {
+        console.log(err);
+      });
+      myJpegTranslator.on('error', function(err) {
+        console.log(err);
+      });
+
+      dcmj2pnm.stdout.pipe(myJpegTranslator).pipe(request(putImageOpt).on('error', function(err) {
+        console.log(err);
+      }));
+    }
+  }));
   res.status('202').end();
 
 });
